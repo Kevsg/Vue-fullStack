e4d97a601b3b2d11dcf150d5e64b8f40e07804d9
Kevsg <31314322+Kevsg@users.noreply.github.com>
2018-06-03 22:32:58 +0700

    01

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combine'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-  res.send({
-    message: 'hello world!'
-  })
-})
+require('./route')(app)
 
-app.listen(process.env.PORT || 8081)
+sequelize.sync().then(() => {
+  app.listen(config.port || 8081)
+  console.log(`Server started on port ${config.port}`)
+})
