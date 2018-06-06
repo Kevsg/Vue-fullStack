77e63ca0bf56435e5b05cb6c14bba343067d3153
Kevsg <31314322+Kevsg@users.noreply.github.com>
2018-06-06 22:41:55 +0700

    sqlite fix

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,5 +1,14 @@
+const {User} = require('../models')
+
 module.exports = {
-  register () {
-      
+  async register (req, res) {
+    try {
+      const user = await User.create(req.body)
+      res.send(user.toJSON())
+    } catch (err) {
+      res.status(400).send({
+        error: 'This email accoung is already in use'
+      })
+    }
   }
 }
