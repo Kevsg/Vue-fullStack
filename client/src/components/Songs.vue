af8688525dc8a46e71e42e2cea40d6b1571d61a5
Kevsg <31314322+Kevsg@users.noreply.github.com>
2018-06-10 21:13:19 +0700

    10

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -3,10 +3,40 @@
   <v-layout>
     <v-flex xs6 offset-xs3>
       <panel title="Songs">
-        <div v-for="song in songs" :key="song.id">
-          {{song.title}} - 
-          {{song.artist}} - 
-          {{song.album}}
+          <v-btn 
+          slot="action" 
+          class="cyan accent-2" 
+          light medium absolute right middle fab  
+          @click="navigateTo({name: 'songs-create'})"> 
+              <v-icon>add</v-icon>
+          </v-btn>
+
+        <div class="song" v-for="song in songs" :key="song.id">
+
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-tile"> {{song.tile}} </div>
+              <div class="song-artist"> {{song.artist}} </div>
+              <div class="song-genre"> {{song.genre}} </div>
+
+                  <v-btn
+                    dark
+                    class="cyan"
+                    @click="navigateTo({
+                      name: 'song',
+                      params: {
+                        songId: song.id
+                      }
+                    })">
+                  View
+                  </v-btn>    
+            </v-flex>
+
+            <v-flex xs6>
+              <img class="album-image" :src="song.albumImageURL"/>
+            </v-flex>
+          </v-layout>
+
         </div>
       </panel>
     </v-flex>
@@ -29,10 +59,38 @@ export default {
     },
     async mounted () {
       this.songs = (await SongsService.index()).data
+    },
+    methods: {
+      navigateTo (route) {
+        this.$router.push(route)
+      }
     }
 }
 
 </script>
 
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+
+.song-title {
+  font-size:  30px;
+}
+
+.song-artist {
+  font-size:  24px;
+}
+
+.song-genre {
+  font-size:  18px;
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
+
 </style>
